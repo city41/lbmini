7a5839652532e9534b232732b990a64be64c5013
Matt Greer <matt.e.greer@gmail.com>
2024-02-22 09:48:59 -0500

    initial commit

---
+++ src/patchRom/types.ts
@@ -0,0 +1,41 @@
+export type PatchDescription = {
+	patchDescription: string;
+};
+
+export type BasePatch = {
+	type: 'crom' | 'prom';
+	description?: string;
+	skip?: boolean;
+};
+
+export type CromPatch = BasePatch & {
+	type: 'crom';
+	imgFile: string;
+	paletteFile: string;
+	destStartingIndex: string;
+};
+
+export type CromBuffer = {
+	fileName: string;
+	data: number[];
+};
+
+export type BasePromPatch = BasePatch & {
+	type: 'prom';
+	symbol?: string;
+};
+
+export type AddressPromPatch = BasePromPatch & {
+	address?: string;
+	subroutine?: boolean;
+	patchAsm: string[];
+};
+
+export type StringPromPatch = BasePromPatch & {
+	string: true;
+	value: string;
+};
+
+export type Patch = AddressPromPatch | StringPromPatch | CromPatch;
+
+export type PatchJSON = Patch[];
